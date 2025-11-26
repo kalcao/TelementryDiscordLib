56ffc929a966624f2d7c228bc4ad007029def6a0
kalcao <lnstagram@kakao.com>
2025-11-27 01:26:03 +0900

    + ev

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from lib.client import DiscordClient
 
 TOKEN = """Your
 Tokens
-Here""".splitlines()
+Here
+:)""".splitlines()
 
 
 async def worker(token, index):
@@ -15,48 +16,58 @@ async def worker(token, index):
         await client.init()
         await client.ws.is_ready.wait()
 
-        # Presence
-        await client.actions.appearance.change_presence(
-            status="dnd",
-            activities=[{"type": 0, "name": "Hi from TDLib!"}]
-        )
+        # # Presence
+        # await client.actions.appearance.change_presence(
+        #     status="dnd",
+        #     activities=[{"type": 0, "name": "Hi from TDLib!"}]
+        # )
         
-        # Guild
-        await client.actions.guild.join("fEvY9J7W")
-        await client.actions.guild.join_vc("Guild Id", "Voice channel ID")
+        # # Guild
+        # await client.actions.guild.join("fEvY9J7W")
+        # await client.actions.guild.join_vc("Guild Id", "Voice channel ID")
 
-        await client.actions.guild.play_soundboard("Channel Id", 2, "🔊") # Play soundboard
-        await client.actions.guild.leave_vc()
+        # await client.actions.guild.play_soundboard("Channel Id", 2, "🔊") # Play soundboard
+        # await client.actions.guild.leave_vc()
 
-        users = await client.actions.guild.scrape("Guild ID", "Channel ID")
+        # users = await client.actions.guild.scrape("Guild ID", "Channel ID")
 
-        # Fetch message from channel and print 50 latest messages
-        channels = await client.actions.misc.fetch_channels("Guild ID")
-        print(await client.actions.misc.fetch_message("Guild ID", channels[0]['id'], limit=50))
+        # # Fetch message from channel and print 50 latest messages
+        # channels = await client.actions.misc.fetch_channels("Guild ID")
+        # print(await client.actions.misc.fetch_message("Guild ID", channels[0]['id'], limit=50))
 
-        # Adding user
-        await client.actions.relationship.add(users[0]['id']) # user id, "not username" !!
+        # # Adding user
+        # await client.actions.relationship.add(users[0]['id']) # user id, "not username" !!
         
-        # Sending DM to a user
-        channel_id = await client.actions.relationship.open_dm("1234567890123456789")['id']
-        await client.actions.misc.send_message(channel_id, "Hi <@1234567890123456789>! This is automated DM from TDLib :)")
+        # # Sending DM to a user
+        # channel_id = await client.actions.relationship.open_dm("1234567890123456789")['id']
+        # await client.actions.misc.send_message(channel_id, "Hi <@1234567890123456789>! This is automated DM from TDLib :)")
 
-        # Profile
-        await client.actions.appearance.change_profile(
-            global_name="NewName",
-            bio=":wave: Hello :)",
-            pronouns="bot/bots"
-        )
+        # # Profile
+        # await client.actions.appearance.change_profile(
+        #     global_name="NewName",
+        #     bio="👋 Hello :)",
+        #     pronouns="bot/bots"
+        # )
 
         # Verifying token with phone number
-        phone_number = input("Phone number >")
-        print(await client.actions.misc.verify.phone.add(phone_number))
+        # phone_number = input("Phone number >")
+        # print(await client.actions.misc.verify.phone.add(phone_number))
 
-        code = input("code> ")
-        pw = input("password >")
+        # code = input("code> ")
+        # pw = input("password >")
         
-        print(await client.actions.misc.verify.phone.verify(phone_number, code, pw))
+        # print(await client.actions.misc.verify.phone.verify(phone_number, code, pw))
         
+        # Verifying token with email
+        #email = input("Email >")
+        #password = input("Password >")
+        #print(await client.actions.misc.verify.email.add(email, password))
+
+        #verifyToken = input("verifyToken> ") # The verify token looks like eyJpZCI6MTQ...
+        #verifyUrl = input("verifyUrl> ")
+        
+        #print(await client.actions.misc.verify.email.verify(verifyToken, verifyUrl))
+
     except Exception as e:
         print(f"[{index}] {e}")
     finally:
